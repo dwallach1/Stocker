ebf483e6fdda03c43137867a824b30955bcd2238
David Wallach <davidwallach2@gmail.com>
2017-08-01 12:44:36 -0700

    corrected __iter__ method for webnode class

--- src/webparser.py
+++ src/webparser.py
@@ -33,9 +33,11 @@ class WebNode(object):
 		"""represents an entry in data.csv that will be used to train the sentiment classifier"""
 		for key, value in kwargs.items():
       		setattr(self, key, value)
-
+	
 	def __iter__(self):
-		attrs = [attr for attr in dir(self) if attr[:2] != '__']
+    		attrs = [attr for attr in dir(self) if attr[:2] != '__']
+    		for attr in attrs:
+      			yield attr, getattr(self, attr)
 
 PriceChange = namedtuple('PriceChange', 'status magnitude')
 
